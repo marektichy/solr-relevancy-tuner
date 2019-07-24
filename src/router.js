8d1b4414e4688a5f4217701caae47669506eb7b5
Marek Tichy <marek.tichy@gmail.com>
2019-07-24 12:47:31 +0200

    npm update

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Tune from './views/Tune.vue'
+import Setup from './views/Setup.vue'
 
 Vue.use(Router)
 
@@ -14,10 +15,11 @@ export default new Router({
     {
       path: '/setup',
       name: 'setup',
+      component: Setup
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "setup" */ './views/Setup.vue')
+      //component: () => import(/* webpackChunkName: "setup" */ './views/Setup.vue')
     }
   ]
 })
